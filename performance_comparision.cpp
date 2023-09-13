66d9660fcd32414232771701ab85dab35eaabb54
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2023-09-13 13:38:58 +0300

    Update performance_comparision.cpp

--- performance_comparision.cpp
+++ performance_comparision.cpp
@@ -2,17 +2,17 @@
 #include <Eigen/Dense>
 #include <chrono>
 
-//method1 = our prediction error method.
-//method2 = matrix inversion method.
-
 int main() {
-    int N = 1000;  // number of the subjects
+
+    std::cout << "Performance Comparsion:" << std::endl;
+    std::cout << "----------------------" << std::endl;
+
+    int N = 10000;  // number of the subjects
     int M = 100;   // number of the variables
 
-    // Seed random number generator
     srand((unsigned int)time(0));
 
-    // Generate random matrices using Eigen
+    // Generate data where ys = ws @ xs 
     Eigen::MatrixXd xs = Eigen::MatrixXd::Random(M, N);
     Eigen::VectorXd ws = Eigen::VectorXd::Random(M);
     Eigen::VectorXd ys = ws.transpose() * xs;
@@ -20,17 +20,15 @@ int main() {
     Eigen::MatrixXd x = xs;  // make copies of xs & ys before modifying them 
     Eigen::VectorXd y = ys;
 
+
+    // method1
     auto start = std::chrono::high_resolution_clock::now();
-
-    Eigen::VectorXd wy = Eigen::VectorXd::Zero(M);
-    Eigen::VectorXd sx = Eigen::VectorXd::Zero(M);
-
+    Eigen::VectorXd wy(M);
+    Eigen::VectorXd sx(M);
     for (int i = 0; i < M; i++) {
         sx(i) = x.row(i).squaredNorm();
-        for (int j = i + 1; j < M; j++) {
-            double wx = (x.row(i) * x.row(j).transpose()).sum() / sx(i);
-            x.row(j) -= wx * x.row(i);
-        }
+        Eigen::VectorXd projection = (x.block(i + 1, 0, M - i - 1, N) * x.row(i).transpose()) / sx(i);
+        x.block(i + 1, 0, M - i - 1, N) -= projection * x.row(i);
     }
 
     for (int i = M - 1; i >= 0; i--) {
@@ -40,21 +38,23 @@ int main() {
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Time method1: " << elapsed.count() << " seconds" << std::endl;
+    std::cout << "Method1: Solving the equation with Error Prediction & Gram-Schmidt orthogonalization" << std::endl;
+    std::cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
 
+    std::cout << "------------------" << std::endl;
+    // method2
     Eigen::MatrixXd ix = xs.transpose();
     start = std::chrono::high_resolution_clock::now();
-
     Eigen::VectorXd wi = ix.completeOrthogonalDecomposition().pseudoInverse() * ys;
-
     end = std::chrono::high_resolution_clock::now();
     elapsed = end - start;
-    std::cout << "Time method2: " << elapsed.count() << " seconds" << std::endl;
+    std::cout << "Method2: Solving the equation using Matrix Inversion & SVD Decompostision" << std::endl;
+    std::cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
 
-    std::cout << "--------------" << std::endl;
-    std::cout << "comparing the resulted weights from the two methods" << std::endl;
-    for (int i = 0; i < 10; i++) {
-        std::cout << "method1: " <<  wy(i) << " - method2: " <<  wi(i) << std::endl;
+    std::cout << "------------------" << std::endl;
+    std::cout << "Comparing a sample of the resulted weights from the two methods (should be similar)" << std::endl;
+    for (int i = 0; i < 5; i++) {
+        std::cout << "method1: " <<  wy(i) << "  method2: " <<  wi(i) << std::endl;
     }
 
     return 0;
